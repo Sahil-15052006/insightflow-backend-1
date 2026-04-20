e6972a36696366e26c7859cf7776988e4f85afe8
Sahil-15052006 <sahilsawant15052006@gmail.com>
2026-04-20 13:12:59 +0530

    fix typo

--- src/routes/datasetRoutes.js
+++ src/routes/datasetRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Dataset = require("../models/Dataset");
+const Dataset = require("../models/Datasets");
 const auth = require("../middleware/auth");
 
 
