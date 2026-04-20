1668e92c27ce589d6ca992524afc7596c548126b
Sahil-15052006 <sahilsawant15052006@gmail.com>
2026-04-20 13:08:58 +0530

    increase data upload limit

--- src/app.js
+++ src/app.js
@@ -11,6 +11,8 @@ const datasetRoutes = require("./routes/datasetRoutes");
 
 const app=express()
 
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.json())
 app.use(cookieParse())
 app.use(cors({
